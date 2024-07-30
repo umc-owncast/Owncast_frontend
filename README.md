a813a5c40e0726f1bee639b5bbdce20455972815
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-07-30 19:46:58 +0900

    signup & login section

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OwnCast Frontend
-# test
+
 
--- app/src/main/java/kr/dori/android/own_cast/SplashActivity.kt
+++ app/src/main/java/kr/dori/android/own_cast/SplashActivity.kt
@@ -1,32 +1,34 @@
 package kr.dori.android.own_cast
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kr.dori.android.own_cast.databinding.ActivitySplashBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-/*
- splash, 3초간 보여짐
- */
 
-class SplashActivity: AppCompatActivity() {
-
-    private lateinit var binding: ActivitySplashBinding
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        if (SignupData.nickname == getString(R.string.signup_info_first)) {
+            MainScope().launch {
+                delay(3000) // 3 seconds delay - now : logout
+                startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                finish()
+            }
+        } else {
+            MainScope().launch {
+                delay(2000) // 2 seconds delay - now : login
+                startActivity(Intent(this@SplashActivity, AuthAfterActivity::class.java))
+                finish()
+            }
+        }
 
-        val handler = Handler(Looper.getMainLooper())
-
-        handler.postDelayed({
-            startActivity(Intent(this,AuthActivity::class.java))
-
-        },3000)
     }
-
 }
