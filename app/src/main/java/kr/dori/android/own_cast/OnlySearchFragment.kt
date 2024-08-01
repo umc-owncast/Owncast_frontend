6d2589664355972544907624814ab76292b39196
koyoungjun <koyoungjunwind@gmail.com>
2024-08-01 17:42:28 +0900

    feat: 검색, 학습, 플리 이벤트 처리

--- app/src/main/java/kr/dori/android/own_cast/OnlySearchFragment.kt
+++ app/src/main/java/kr/dori/android/own_cast/OnlySearchFragment.kt
@@ -5,26 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.dori.android.own_cast.databinding.FragmentSearchSaveCategoryBinding
+import kr.dori.android.own_cast.databinding.FragmentOnlySearchBinding
 
-
-class SearchSaveCategoryFragment : Fragment() {
-    lateinit var binding: FragmentSearchSaveCategoryBinding
-
+class OnlySearchFragment : Fragment() {
+    lateinit var binding: FragmentOnlySearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSearchSaveCategoryBinding.inflate(inflater,container,false)
-        // Inflate the layout for this fragment
 
-        binding.fragmentSearchSaveCategoryBack.setOnClickListener {
+        binding = FragmentOnlySearchBinding.inflate(inflater, container, false)
+
+        binding.fragmentOnlySearchBack.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
 
+        // Inflate the layout for this fragment
         return binding.root
-
     }
 
 
--- app/src/main/java/kr/dori/android/own_cast/SearchFragment.kt
+++ app/src/main/java/kr/dori/android/own_cast/SearchFragment.kt
@@ -5,55 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kr.dori.android.own_cast.databinding.FragmentPlaylistBinding
+import kr.dori.android.own_cast.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentSearchBinding
+    private val saveFragment = SearchSaveCategoryFragment()
+    private val onlySearchFragment = OnlySearchFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+/*          데이터 전달 부분: 카테고리 데이터 받아와서 캐스트 정보 전달
+            val bundle = Bundle().apply {
+
+                putParcelableArrayList("isSave", ArrayList(sharedViewModel.data.value?.filter { it.isSave } ?: listOf()))
             }
+            castFragment.arguments = bundle
+
+ */
+            /* 이 부분을 리사이클러뷰 어댑터에 설정해야 됨
+
+        binding..setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, saveFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+                 */
+        binding.fragmentSearch.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm,onlySearchFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+
+        return binding.root
     }
 }
