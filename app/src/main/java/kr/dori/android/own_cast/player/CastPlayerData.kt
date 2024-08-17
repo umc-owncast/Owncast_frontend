8d20984a50e5b7fd905cebf2f771a3ba3cf9ab82
koyoungjun <koyoungjunwind@gmail.com>
2024-08-18 06:42:01 +0900

    fix: next, previous event -  싱글톤에 맞춰서 로직을 변경했습니다. - 원래는 받아온 데이터의 리스트 안에서만 이동이 가능했지만, 싱글톤으로 변경하면서 스포티파이의 재생기록처럼 사용자가 접근한 모든 재생기록으로 돌아갈 수 있습니다.

--- app/src/main/java/kr/dori/android/own_cast/player/CastPlayerData.kt
+++ app/src/main/java/kr/dori/android/own_cast/player/CastPlayerData.kt
@@ -16,21 +16,49 @@ object CastPlayerData {
     fun test(testList: List<Cast>) {
         test.addAll(testList)
 
-        // testPosition 계산 후 유효한지 확인합니다.
+        // testPosition 계산 후 유효한지 확인합니다. -> position의 초기 설정입니다.
         testPosition = test.size - testList.size
 
         if (testPosition in 0 until test.size) {
             testCast = test[testPosition]
-            Log.d("test", "${testList.size}, ${test.size}, ${testCast}, ${test}")
+            Log.d("test", "testPosition: ${testPosition}, testList.size: ${testList.size}, test.size: ${test.size}, ${testCast}, ${test}")
         } else {
             Log.e("test", "Invalid testPosition: $testPosition, testList size: ${testList.size}, ${test.size},${testPosition}")
         }
     }
 
 
+
+    fun playNext(): Cast? {
+        return if (testPosition + 1 in 0 until test.size) {
+            testPosition++
+            testCast = test[testPosition]  // 업데이트된 testPosition 값을 사용하여 testCast를 업데이트합니다.
+            testCast
+
+        } else {
+            Log.e("playNext", "Next position is out of bounds: $testPosition")
+            null
+        }
+    }
+
+    fun playPrevious(): Cast? {
+        return if (testPosition - 1 in 0 until test.size){
+            testPosition--
+            testCast = test[testPosition]
+            testCast
+    } else {
+        Log.e("playPrevious","Previous position is out of bounds: $testPosition")
+        null
+    }
+    }
+
+
+/*
     val currentCast: Cast?
         get() = if (currentIndex in castList.indices) castList[currentIndex] else null
 
+
+ */
     fun setCastList(newCastList: List<Cast>) {
         castList.clear()
         castList.addAll(newCastList)
@@ -46,25 +74,7 @@ object CastPlayerData {
         resetPlaybackState()
     }
 
-    fun playNext(): Cast? {
-        return if (currentIndex + 1 in castList.indices) {
-            currentIndex++
-            resetPlaybackState()
-            currentCast
-        } else {
-            null
-        }
-    }
 
-    fun playPrevious(): Cast? {
-        return if (currentIndex - 1 >= 0) {
-            currentIndex--
-            resetPlaybackState()
-            currentCast
-        } else {
-            null
-        }
-    }
 
     fun updatePlaybackPosition(position: Long) {
         currentPosition = position
