5a71a8d7e9e34e7861bcb60d73efcd849bb9a0d2
koyoungjun <koyoungjunwind@gmail.com>
2024-08-18 05:48:58 +0900

    feat: 싱글톤으로 캐스트 데이터 관리 - CurrentCast를 통해서 현재 재생되는 캐스트를 List에서 정확하게 찾을 수 있습니다. - 캐스트를 하나만 넘겨주는 상황이 있고 플레이리스트에서 캐스트의 리스트를 넘겨주는 경우가 있는데, 상황에 맞춰서 동작하도록 했습니다.

--- app/src/main/java/kr/dori/android/own_cast/player/CastPlayerData.kt
+++ app/src/main/java/kr/dori/android/own_cast/player/CastPlayerData.kt
@@ -1,5 +1,6 @@
 package kr.dori.android.own_cast.data
 
+import android.util.Log
 import kr.dori.android.own_cast.forApiData.Cast
 
 object CastPlayerData {
@@ -7,6 +8,25 @@ object CastPlayerData {
     private var currentIndex = -1
     var currentPosition: Long = 0L
     var playbackSpeed: Float = 1.0f
+
+    private val test = mutableListOf<Cast>()
+    var testPosition : Int = 0
+    lateinit var testCast: Cast
+
+    fun test(testList: List<Cast>) {
+        test.addAll(testList)
+
+        // testPosition 계산 후 유효한지 확인합니다.
+        testPosition = test.size - testList.size
+
+        if (testPosition in 0 until test.size) {
+            testCast = test[testPosition]
+            Log.d("test", "${testList.size}, ${test.size}, ${testCast}, ${test}")
+        } else {
+            Log.e("test", "Invalid testPosition: $testPosition, testList size: ${testList.size}, ${test.size},${testPosition}")
+        }
+    }
+
 
     val currentCast: Cast?
         get() = if (currentIndex in castList.indices) castList[currentIndex] else null
