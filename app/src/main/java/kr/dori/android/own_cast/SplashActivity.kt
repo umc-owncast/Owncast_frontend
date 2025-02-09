02ae6add67e4921d3a477f0906d04071ca00c52e
younheejae <hjy10045@sookmyung.ac.kr>
2025-02-10 02:41:14 +0900

    fix #65 회원가입 화면 수정

--- app/src/main/java/kr/dori/android/own_cast/SplashActivity.kt
+++ app/src/main/java/kr/dori/android/own_cast/SplashActivity.kt
@@ -2,6 +2,7 @@ package kr.dori.android.own_cast
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
@@ -12,31 +13,117 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthError
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kr.dori.android.own_cast.databinding.ActivitySplashBinding
+import kr.dori.android.own_cast.presentation.ui.auth.social.SocialLoginUiState
+import kr.dori.android.own_cast.presentation.ui.auth.social.SocialLoginViewModel
 import kr.dori.android.own_cast.signUp.SignupFirstActivity
 
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val socialLoginViewModel by viewModels<SocialLoginViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
         enableEdgeToEdge()
 
         // 이미지 이동 애니메이션 실행
         moveImageViewOffScreen()
 
-        // 로그인 버튼 클릭 이벤트 설정
-        findViewById<Button>(R.id.loginBtn).setOnClickListener {
+        addListeners()
+        socialLogin()
+    }
+
+    private fun addListeners() {
+        // 로그인
+        binding.loginBtn.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
-
-        // 회원가입 버튼 클릭 이벤트 설정
-        findViewById<Button>(R.id.signUpBtn).setOnClickListener {
+        // 회원가입
+        binding.signUpBtn.setOnClickListener {
             startActivity(Intent(this, SignupFirstActivity::class.java))
+        }
+        // 카카오 로그인
+        binding.btnKakaoLogin.setOnClickListener {
+            socialLoginViewModel.kakaoLogin()
+        }
+    }
+
+    private fun socialLogin() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                socialLoginViewModel.socialLoginUiState.collect() { uiState ->
+                    when(uiState) {
+                        SocialLoginUiState.KakaoLogin -> {
+                            handleKakaoLogin()
+                            socialLoginViewModel.setUiStateIdle()
+                        }
+                        SocialLoginUiState.KakaoLoginSuccess -> {
+                            // 사용자의 정보 얻어옴
+                        }
+                        SocialLoginUiState.LoginFail -> {
+                        }
+                        else -> {
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleKakaoLogin() {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if(error != null) {
+                Log.e("handleKakaoLogin", "카카오계정 로그인 실패 $error")
+//                if(error.toString().contains("statusCode=302")) {
+//                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+//                }
+                socialLoginViewModel.kakaoLoginFail()
+            } else if(token != null) {
+                Log.e("handleKakaoLogin", "카카오계정 로그인 성공 ${token.accessToken}")
+                socialLoginViewModel.kakaoLoginSuccess()
+            }
+        }
+
+        // 카카오톡 설치 확인
+        if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                if(error != null) {
+                    Log.e("handleKakaoLogin", "카카오계정 로그인 실패 $error")
+
+                    // 사용자 취소
+                    if(
+                        (error is ClientError && error.message?.contains("user cancelled") == true) ||
+                        (error is AuthError && error.message?.contains("User denied access") == true)
+                    ) {
+                        socialLoginViewModel.kakaoLoginFail()
+                        return@loginWithKakaoTalk
+                    }
+                    // 카카오톡에 연결된 카카오계정이 없는 경우 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                } else if(token != null) {
+                    Log.i("handleKakaoLogin", "카카오계정 로그인 성공 ${token.accessToken}")
+                    socialLoginViewModel.kakaoLoginSuccess()
+                }
+            }
+        }
+        else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
         }
     }
 
@@ -133,6 +220,8 @@ class SplashActivity : AppCompatActivity() {
             // 로그인, 회원가입 버튼 표시
             loginBtn.visibility = View.VISIBLE
             signUpBtn.visibility = View.VISIBLE
+            binding.btnKakaoLogin.visibility = View.VISIBLE
+            binding.btnNaverLogin.visibility = View.VISIBLE
             addText.visibility = View.VISIBLE
             loginBtn.startAnimation(fadeIn)
             signUpBtn.startAnimation(fadeIn)
